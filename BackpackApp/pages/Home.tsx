7966d91567732c23cf955b150801f18d5c833c4b
Mace <153683321+127msafran@users.noreply.github.com>
2026-01-13 18:36:16 +0000

    Add Home.tsx file

---
+++ BackpackApp/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Pressable, Modal } from 'react-native';
+import { useState } from 'react';
+
+export default function Home() {
+    
+
+    return (
+        <View>
+            <Text>Home Page</Text>
+        </View>
+    );
+}
