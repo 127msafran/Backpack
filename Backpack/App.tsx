0fb7535e7dcb6c678a96e171bb40dda583785e0d
Mace <153683321+127msafran@users.noreply.github.com>
2026-02-02 15:32:04 +0000

    Add some stuff

--- Backpack/App.tsx
+++ Backpack/App.tsx
@@ -1,71 +1,64 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable, Modal, Platform } from 'react-native';
+import { Platform, Image } from 'react-native';
 import { createNativeBottomTabNavigator } from '@react-navigation/bottom-tabs/unstable';
 import { NavigationContainer } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 import Assignments from './pages/Assignments';
 import Home from './pages/Home';
 import Schedule from './pages/Schedule';
 
 export default function App() {
   const NativeTabs = createNativeBottomTabNavigator();
-  const [color, setColor] = useState('red');
 
   return (
-    <NavigationContainer>
+    <>
       <StatusBar style="auto" />
-        <NativeTabs.Navigator initialRouteName="Home">
-          <NativeTabs.Screen name="Home" component={Home} options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: Platform.select({
-              ios: {
-                type: 'sfSymbol',
-                name: 'house',
-              },
-              android: {
-                type: 'drawableResource',
-                name: 'heart_icon',
-              }
-            }),
-            tabBarActiveTintColor: 'red',
+      <NavigationContainer>
+          <NativeTabs.Navigator initialRouteName="Home">
+            <NativeTabs.Screen name="Home" component={Home} options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: Platform.select({
+                ios: {
+                  type: 'sfSymbol',
+                  name: 'house',
+                },
+                android: {
+                  type: 'image',
+                  source: require('./assets/home_icon.png'),
+                }
+              }),
+              tabBarActiveTintColor: 'red',
+            }} />
+            <NativeTabs.Screen name="Assignments" component={Assignments} options={{
+              tabBarLabel: 'Assignments',
+              tabBarIcon: Platform.select({
+                ios: {
+                  type: 'sfSymbol',
+                  name: 'doc.text',
+                }, android: {
+                  type: 'drawableResource',
+                  name: 'heart_icon',
+                },
+              }),
+              tabBarActiveTintColor: 'red',
+            }} />
+            <NativeTabs.Screen name="Schedule" component={Schedule} options={{
+              tabBarLabel: 'Schedule',
+              tabBarIcon: Platform.select({
+                ios: {
+                  type: 'sfSymbol',
+                  name: 'calendar',
+                },
+                android: {
+                  type: 'drawableResource',
+                  name: 'heart_icon',
+                },
+              }),
+              tabBarActiveTintColor: 'red',
           }} />
-          <NativeTabs.Screen name="Assignments" component={Assignments} options={{
-            tabBarLabel: 'Assignments',
-            tabBarIcon: Platform.select({
-              ios: {
-                type: 'sfSymbol',
-                name: 'doc.text',
-              }, android: {
-                type: 'drawableResource',
-                name: 'heart_icon',
-              },
-            }),
-            tabBarActiveTintColor: 'red',
-          }} />
-          <NativeTabs.Screen name="Schedule" component={Schedule} options={{
-            tabBarLabel: 'Schedule',
-            tabBarIcon: Platform.select({
-              ios: {
-                type: 'sfSymbol',
-                name: 'calendar',
-              },
-              android: {
-                type: 'drawableResource',
-                name: 'heart_icon',
-              },
-            }),
-            tabBarActiveTintColor: 'red',
-        }} />
-        </NativeTabs.Navigator>
-    </NavigationContainer>
+          </NativeTabs.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
